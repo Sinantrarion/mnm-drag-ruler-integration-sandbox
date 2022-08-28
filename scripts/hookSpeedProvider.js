4536991327918708802da2c4d425d24e9f6478f2
Sinantrarion <vikbar199@gmail.com>
2022-08-28 14:02:28 +0300

    Working hopefully

--- scripts/hookSpeedProvider.js
+++ scripts/hookSpeedProvider.js
@@ -1,26 +1,20 @@
 Hooks.once("dragRuler.ready", (SpeedProvider) => {
-    class ShadowrunSpeedProvider extends SpeedProvider {
+    class MnMSpeedProvider extends SpeedProvider {
         get colors() {
             return [
-                {id: "walk", default: 0x00FF00, name: "shadowrun5e.speeds.walk"},
-                {id: "dash", default: 0xFFFF00, name: "shadowrun5e.speeds.dash"},
-                {id: "run", default: 0xFF8000, name: "shadowrun5e.speeds.run"}
+                {id: "walk", default: 0x00FF00, name: "MnM.speeds.walk"}
             ]
         }
 
         getRanges(token) {
-            const baseSpeed = token.actor.data.data.movement.walk.value
-            const runSpeed = token.actor.data.data.movement.run.value
-            const sprintSpeed = token.actor.data.data.movement.sprint.value
+            const baseSpeed = token.actor.data.data.attributes.NUM_SPEED_DISTANCE.value
 
-            // Characters walk up to agi*2 unless thats modified, and sprint with agi*4, so lets just take the final value
             const ranges = [
-                {range: baseSpeed, color: "walk"},
-                {range: runSpeed, color: "dash"}
+                {range: baseSpeed, color: "walk"}
             ]
 
             return ranges
         }
     }
-    dragRuler.registerModule("drag-ruler-integration-for-shadowrun-5e", ShadowrunSpeedProvider)
+    dragRuler.registerModule("mnm-drag-ruler-integration-sandbox", MnMSpeedProvider)
 })
