df6783b135a6796cc6af55ba2fecc0af016de441
Sinantrarion <vikbar199@gmail.com>
2022-08-28 19:14:43 +0300

    Updates

--- scripts/hookSpeedProvider.js
+++ scripts/hookSpeedProvider.js
@@ -5,21 +5,21 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
                 {id: "MoveAction", default: 0x04D40B},
                 {id: "Athletic", default: 0xF1F505},
                 {id: "ActionDash", default: 0x0505F5},
-            ];
+            ]
         }
 
         getRanges(token) {
-            const baseSpeed = token.actor.data.data.attributes.NUM_SPEED_DISTANCE.value;
+            const baseSpeed = token.actor.data.data.attributes.NUM_SPEED_DISTANCE;
 
             const ranges = [
                 {range: baseSpeed, color: "walk"},
                 {range: baseSpeed * 2, color: "Athletic"},
                 {range: baseSpeed * 4, color: "ActionDash"}
 				
-            ];
+            ]
 
-            return ranges;
+            return ranges
         }
     }
-    dragRuler.registerModule("mnm-drag-ruler-integration-sandbox", MnMSpeedProvider);
+    dragRuler.registerModule("mnm-drag-ruler-integration-sandbox", MnMSpeedProvider)
 })
