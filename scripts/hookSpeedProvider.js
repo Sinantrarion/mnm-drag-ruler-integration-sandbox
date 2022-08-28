ba14bf3a72f12789198ddfbb23352c01773b0c1f
Sinantrarion <vikbar199@gmail.com>
2022-08-28 18:38:17 +0300

    b?

--- scripts/hookSpeedProvider.js
+++ scripts/hookSpeedProvider.js
@@ -2,19 +2,25 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
     class MnMSpeedProvider extends SpeedProvider {
         get colors() {
             return [
-                {id: "walk", default: 0x00FF00, name: "MnM.speeds.walk"}
-            ]
+                {id: "MoveAction", default: 0x04D40B},
+                {id: "Athletic", default: 0xF1F505},
+                {id: "ActionDash", default: 0x0505F5},
+            ];
         }
 
         getRanges(token) {
-            const baseSpeed = token.actor.data.data.attributes.NUM_SPEED_DISTANCE.value;
+            const movement = token.actor.data.data.attributes.NUM_SPEED_DISTANCE.value;
+			const baseSpeed = Math.max(...Object.values(movement));
 
             const ranges = [
-                {range: baseSpeed, color: "walk"}
-            ]
+                {range: baseSpeed, color: "walk"},
+                {range: baseSpeed * 2, color: "Athletic"},
+                {range: baseSpeed * 4, color: "ActionDash"}
+				
+            ];
 
-            return ranges
+            return ranges;
         }
     }
-    dragRuler.registerModule("mnm-drag-ruler-integration-sandbox", MnMSpeedProvider)
+    dragRuler.registerModule("mnm-drag-ruler-integration-sandbox", MnMSpeedProvider);
 })
